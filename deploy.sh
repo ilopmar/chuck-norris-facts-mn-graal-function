d5d33b2c433ca7cb6dadf57543cd4d8814581cad
Iván López <lopez.ivan@gmail.com>
2020-01-22 10:16:34 +0100

    Use cloudformation to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,8 @@ docker build . -t chuck-norris-facts
 mkdir -p build
 docker run --rm --entrypoint cat chuck-norris-facts  /home/application/function.zip > build/function.zip
 
-# check for role
-#ROLE_NAME=lambda-basic-role
-#ROLE_ARN=`aws iam get-role --role-name ${ROLE_NAME} | grep Arn | cut -d'"' -f4`
-#if [ -z "$ROLE_ARN" ]; then
-#    echo "No role ${ROLE_NAME} exists!"
-#    echo "Create one using: "
-#    echo "> aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file://lambda-role-policy.json"
-#    echo "> aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-#    exit 1
-#fi
-
-#aws lambda create-function --function-name chuck-norris-facts \
-#--zip-file fileb://build/function.zip --handler function.handler --runtime provided \
-#--role ${ROLE_ARN}
-
 S3_BUCKET="oci-bucket-ilopmar"
-STACK_NAME="ChuckNorrisFactsMnFunction"
+STACK_NAME="ChuckNorrisFactsMnFunction1931"
 
 aws cloudformation package --template-file sam.yaml --output-template-file output-sam.yaml --s3-bucket $S3_BUCKET
 
@@ -27,4 +12,5 @@ aws cloudformation deploy --template-file output-sam.yaml --stack-name $STACK_NA
 
 aws cloudformation describe-stacks --stack-name $STACK_NAME
 
+# To delete everything
 #aws cloudformation delete-stack --stack-name $STACK_NAME
